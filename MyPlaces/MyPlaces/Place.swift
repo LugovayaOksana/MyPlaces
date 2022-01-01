73e1e885b05617ee8b8566994250fa6144c07b47
Oksana <hrlugovaya@gmail.com>
2022-01-01 18:12:19 +0300

    add RealmSwift

---
+++ MyPlaces/MyPlaces/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  MyPlaces
+//
+//  Created by Оксана on 30.12.2021.
+//
+
+import Foundation
---
+++ MyPlaces/MyPlaces/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Оксана on 01.01.2022.
+//
+
+import Foundation
