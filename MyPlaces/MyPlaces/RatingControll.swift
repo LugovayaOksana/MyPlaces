f93c3b5ef9b73ae832e92a7f54fd7bd1bd73d419
Oksana Lugovaya <hrlugovaya@gmail.com>
2022-01-03 19:15:16 +0300

    add rating

---
+++ MyPlaces/MyPlaces/RatingControll.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControll.swift
+//  MyPlaces
+//
+//  Created by Оксана on 03.01.2022.
+//
+
+import UIKit
+
+class RatingControll: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
